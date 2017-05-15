2f21a4df2916634e6a37d9ee1c1f0273d82079ee
Unknown <logmister@gmail.com>
2017-05-15 14:21:30 -0700

    inspire section build

--- page-templates/front.php
+++ page-templates/front.php
@@ -154,56 +154,39 @@ get_header(); ?>
 			<?php comments_template(); ?>
 			<?php do_action( 'foundationpress_page_after_comments' ); ?>
 		</div>
-
 	</div>
 
 </section>
 <?php endwhile;?>
 <?php do_action( 'foundationpress_after_content' ); ?>
 
+<section class="inspire">
+	<h1 class="text-center">TAKE ACTION</h1>
+	<div class="row">
+		<div class="large-2 columns">
+
+		</div>
+		<figure class="small-12 large-2 columns text-center">
+			<a href="#"><img src="https://placehold.it/240x240" alt="">
+			<figcaption>Volunteer</figcaption></a>
+		</figure>
+		<figure class="small-12 large-4 columns text-center">
+			<a href="#"><img src="https://placehold.it/240x240" alt="">
+			<figcaption>Donation</figcaption></a>
+		</figure>
+		<figure class="small-12 large-2 columns text-center">
+			<a href="#"><img src="https://placehold.it/240x240" alt="">
+			<figcaption>In-kind donation</figcaption></a>
+		</figure>
+		<div class="large-2 columns">
+
+		</div>
+
+	</div>
+</section>
+
 <div class="section-divider">
 	<hr />
 </div>
-
-
-<section class="benefits">
-	<header>
-		<h2>Build Foundation based sites, powered by WordPress</h2>
-		<h4>Foundation is the professional choice for designers, developers and teams. <br /> WordPress is by far, <a href="http://trends.builtwith.com/cms">the world's most popular CMS</a> (currently powering 38% of the web).</h4>
-	</header>
-
-	<div class="semantic">
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/demo/semantic.svg" alt="semantic">
-		<h3>Semantic</h3>
-		<p>Everything is semantic. You can have the cleanest markup without sacrificing the utility and speed of Foundation.</p>
-	</div>
-
-	<div class="responsive">
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/demo/responsive.svg" alt="responsive">
-		<h3>Responsive</h3>
-		<p>You can build for small devices first. Then, as devices get larger and larger, layer in more complexity for a complete responsive design.</p>
-
-	</div>
-
-	<div class="customizable">
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/demo/customizable.svg" alt="customizable">
-		<h3>Customizable</h3>
-		<p>You can customize your build to include or remove certain elements, as well as define the size of columns, colors, font size and more.</p>
-
-	</div>
-
-	<div class="professional">
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/demo/professional.svg" alt="professional">
-		<h3>Professional</h3>
-		<p>Millions of designers and developers depend on Foundation. We have business support, training and consulting to help grow your product or service.</p>
-	</div>
-
-	<div class="why-foundation">
-		<a href="/kitchen-sink">See what's in Foundation out of the box →</a>
-	</div>
-
-</section>
-
-
 
 <?php get_footer();
