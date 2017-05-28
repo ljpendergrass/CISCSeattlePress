35fcdf9ad4263a50bbe1f9a9c551f4b07034f999
Unknown <logmister@gmail.com>
2017-05-28 12:33:17 -0700

    topbar search

--- topbar-searchform.php
+++ topbar-searchform.php
@@ -7,10 +7,11 @@
  */
 
 do_action( 'foundationpress_before_searchform' ); ?>
-<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+<form role="search" method="get" id="searchform" class="flex-container" action="<?php echo home_url( '/' ); ?>">
 	<?php do_action( 'foundationpress_searchform_top' ); ?>
-	<div class="input-group">
-		<i id="searchtoggle" class="fa fa-2x fa-search" aria-hidden="true"></i><input type="text" class="input-group-field" value="" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', 'foundationpress' ); ?>">
+	<i data-toggle="searchtoggle searchicon" id="searchicon" data-toggler=".hide" class="fa fa-2x fa-search" aria-hidden="true"></i>
+  <div data-toggler=".hide" id="searchtoggle" class="input-group hide">
+<input type="text" class="input-group-field" value="" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', 'foundationpress' ); ?>">
 		<?php do_action( 'foundationpress_searchform_before_search_button' ); ?>
 		<div class="input-group-button">
 			<input type="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'foundationpress' ); ?>" class="button">
