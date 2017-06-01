d60ed766d1e835fb4faa7588db83e0c1418e2504
ljpendergrass <logmister@gmail.com>
2017-06-01 01:19:48 -0700

    shortcodes & random fixes

--- library/shortcodes.php
+++ library/shortcodes.php
@@ -25,7 +25,8 @@ function orbit_func( $atts ) {
     'bar' => 'something else',
   ), $atts );
   $t = get_bloginfo('template_directory');
-  $r = '<div class="orbit" role="region" aria-label="Main Images" data-orbit>
+  $r = '<section class="container">
+  <div class="orbit" role="region" aria-label="Main Images" data-orbit>
     	<ul class="orbit-container">
     		<button class="orbit-previous"><span class="show-for-sr">Previous Slide</span> <i class="fa fa-3x fa-angle-left" aria-hidden="true"></i></button>
     		<button class="orbit-next"><span class="show-for-sr">Next Slide</span> <i class="fa fa-3x fa-angle-right" aria-hidden="true"></i></button>
@@ -70,6 +71,7 @@ function orbit_func( $atts ) {
     	  <button data-slide="3"><span class="show-for-sr">Fourth slide details.</span></button>
     	</nav>
     </div>
+    </section>
     <!-- end orbit -->';
   return $r;
  }
@@ -84,63 +86,141 @@ function help_func( $atts ) {
   ), $atts );
   $t = get_bloginfo('template_directory');
 
-  return '<section class="help-intro">
+  return "<section class='help-intro container'>
   	<!-- using tabs to accordion responsive foundation element  -->
   	<!-- TODO: add flex style in order to stretch list in reference to # lis in UL -->
-  	<ul class="accordion intro-flex-container" data-responsive-accordion-tabs="tabs small-accordion medium-accordion large-tabs">
-  	  <li class="accordion-item large-flex-child-auto is-active" data-accordion-item >
-  	    <a href="#" class="accordion-title">
-  				<img src="' . $t . '/assets/images/demo/intro-early-ed.png" alt="Space">
+  	<ul class='accordion intro-flex-container' data-responsive-accordion-tabs='tabs small-accordion medium-accordion large-tabs'>
+  	  <li class='accordion-item large-flex-child-auto is-active' data-accordion-item >
+  	    <a href='#' class='accordion-title'>
+  				<img src='{$t}/assets/images/demo/intro-early-ed.png' alt='Space'>
   				Early Childhood Education</a>
-  	    <div class="accordion-content" data-tab-content>
-  	      A short description about Early Childhood Education goes here. <a href="#" class="button small">LEARN MORE</a>
+  	    <div class='accordion-content' data-tab-content>
+  	      A short description about Early Childhood Education goes here. <a href='#' class='button small'>LEARN MORE</a>
   	    </div>
   	  </li>
-  		<li class="accordion-item large-flex-child-auto" data-accordion-item>
-  			<a href="#" class="accordion-title">
-  				<img src="' . $t . '/assets/images/demo/intro-youth.png" alt="Space">
+  		<li class='accordion-item large-flex-child-auto' data-accordion-item>
+  			<a href='#' class='accordion-title'>
+  				<img src='{$t}/assets/images/demo/intro-youth.png' alt='Space'>
   				Youth Program</a>
-  	    <div class="accordion-content" data-tab-content>
-  				Youth Program description. <a href="#" class="button small">LEARN MORE</a>
+  	    <div class='accordion-content' data-tab-content>
+  				Youth Program description. <a href='#' class='button small'>LEARN MORE</a>
   	    </div>
   	  </li>
-  		<li class="accordion-item large-flex-child-auto" data-accordion-item>
-  			<a href="#" class="accordion-title">
-  				<img src="' . $t . '/assets/images/demo/intro-family.png" alt="Space">
+  		<li class='accordion-item large-flex-child-auto' data-accordion-item>
+  			<a href='#' class='accordion-title'>
+  				<img src='{$t}/assets/images/demo/intro-family.png' alt='Space'>
   				Family Program</a>
-  			<div class="accordion-content" data-tab-content>
-  				Family Program description. <a href="#" class="button small">LEARN MORE</a>
+  			<div class='accordion-content' data-tab-content>
+  				Family Program description. <a href='#' class='button small'>LEARN MORE</a>
   			</div>
   		</li>
-  		<li class="accordion-item large-flex-child-auto" data-accordion-item>
-  			<a href="#" class="accordion-title">
-  				<img src="' . $t . '/assets/images/demo/intro-senior.png" alt="Space">
+  		<li class='accordion-item large-flex-child-auto' data-accordion-item>
+  			<a href='#' class='accordion-title'>
+  				<img src='{$t}/assets/images/demo/intro-senior.png' alt='Space'>
   				Senior Program</a>
-  			<div class="accordion-content" data-tab-content>
-  				Senior Program description. <a href="#" class="button small">LEARN MORE</a>
+  			<div class='accordion-content' data-tab-content>
+  				Senior Program description. <a href='#' class='button small'>LEARN MORE</a>
   			</div>
   		</li>
-  		<li class="accordion-item large-flex-child-auto" data-accordion-item>
-  			<a href="#" class="accordion-title">
-  				<img src="' . $t . '/assets/images/demo/intro-health.png" alt="Space">
+  		<li class='accordion-item large-flex-child-auto' data-accordion-item>
+  			<a href='#' class='accordion-title'>
+  				<img src='{$t}/assets/images/demo/intro-health.png' alt='Space'>
   				Health Care Access</a>
-  			<div class="accordion-content" data-tab-content>
-  				Health Care Access description. <a href="#" class="button small">LEARN MORE</a>
+  			<div class='accordion-content' data-tab-content>
+  				Health Care Access description. <a href='#' class='button small'>LEARN MORE</a>
   			</div>
   		</li>
-  		<li class="accordion-item large-flex-child-auto" data-accordion-item>
-  			<a href="#" class="accordion-title">
-  				<img src="' . $t . '/assets/images/demo/intro-crime.png" alt="Space">
+  		<li class='accordion-item large-flex-child-auto' data-accordion-item>
+  			<a href='#' class='accordion-title'>
+  				<img src='{$t}/assets/images/demo/intro-crime.png' alt='Space'>
   				Crime Victim Advocacy Program</a>
-  			<div class="accordion-content" data-tab-content>
-  				Crime Victim Advocacy description. <a href="#" class="button small">LEARN MORE</a>
+  			<div class='accordion-content' data-tab-content>
+  				Crime Victim Advocacy description. <a href='#' class='button small'>LEARN MORE</a>
   			</div>
   		</li>
 
 
 
   	</ul>
-  </section>';
+  </section>";
  }
  add_shortcode( 'help', 'help_func' );
 // end test
+
+
+// [takeaction title="Take Action" a="postid" b="postid" c="postid"]
+function takeaction_func( $atts ) {
+  $a = shortcode_atts( array( // inputs are taken in; including all of them is optional
+    'title' => "TAKE ACTION",
+    'link_a' => 1,
+    'link_b' => 2,
+    'link_c' => 3,
+  ), $atts );
+  $t = get_bloginfo('template_directory');
+  $url_a = get_permalink( $a['link_a'] );
+  $url_b = get_permalink( $a['link_b'] );
+  $url_c = get_permalink( $a['link_c'] );
+  $title_a = get_the_title( $a['link_a'] );
+  $title_b = get_the_title( $a['link_b'] );
+  $title_c = get_the_title( $a['link_c'] );
+
+  return "<section class='inspire fullblock'>
+    <h1 class='text-center'>{$a['title']}</h1>
+    <div class='row'>
+      <div class='large-2 columns'>
+        &nbsp;
+      </div>
+      <figure class='small-12 medium-3 large-2 columns text-center'>
+        <a href='{$url_a}'><img src='{$t}/assets/images/demo/action-volunteer.png' alt=' '>
+        <figcaption>{$title_a}</figcaption></a>
+      </figure>
+      <figure class='small-12 medium-3 large-4 columns text-center'>
+        <a href='{$url_b}'><img src='{$t}/assets/images/demo/action-donation.png' alt=' '>
+        <figcaption>{$title_b}</figcaption></a>
+      </figure>
+      <figure class='small-12 medium-3 large-2 columns text-center'>
+        <a href='{$url_c}'><img src='{$t}/assets/images/demo/action-inkind.png' alt=' '>
+        <figcaption>{$title_c}</figcaption></a>
+      </figure>
+      <div class='large-2 columns'>
+      &nbsp;
+      </div>
+
+    </div>
+  </section>";
+ }
+ add_shortcode( 'takeaction', 'takeaction_func' );
+// end takeaction
+
+// [contact foo="foo-value"]
+function contact_func( $atts ) {
+  $a = shortcode_atts( array( // inputs are taken in; including all of them is optional
+    'foo' => 'something',
+    'bar' => 'something else',
+  ), $atts );
+  return '<section class="container">
+  	<h2 class="text-center">Stay in touch</h2>
+  	<div class="row">
+  		<div class="small-12 medium-6 large-6 columns">
+  			<ul class="menu centered vertical mainpage-info no-bullet align-center">
+  				<li>
+  					<a href="#"><i class="fa fa-fw fa-address-book green" aria-hidden="true"></i> <span>Program Contacts</span> &nbsp; <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
+  				</li>
+  				<li>
+  					<a href="#"><i class="fa fa-fw fa-map-marker green" aria-hidden="true"></i><span>Site Locations</span> &nbsp; <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
+  				</li>
+  			</ul>
+  		</div>
+  		<div class="small-12 medium-6 large-6 columns subscribe-box">
+  			<h3>Sign Up</h3>
+  			<p>Receive news and invitations</p>
+  			<label>Email
+          <input type="email" placeholder="Enter your email address">
+        </label>
+  			<button type="button" name="button" class="button large">Subscribe</button>
+  		</div>
+  	</div>
+  </section>';
+ }
+ add_shortcode( 'contact', 'contact_func' );
+// end contact
--- page-templates/front.php
+++ page-templates/front.php
@@ -5,7 +5,6 @@ Template Name: Front
 get_header(); ?>
 
 
-<!-- end intro -->
 <?php do_action( 'foundationpress_before_content' ); ?>
 <?php while ( have_posts() ) : the_post(); ?>
 <section class="intro" role="main">
@@ -32,54 +31,4 @@ get_header(); ?>
 </section>
 <?php endwhile;?>
 <?php do_action( 'foundationpress_after_content' ); ?>
-
-<section class="inspire fullblock">
-	<h1 class="text-center">TAKE ACTION</h1>
-	<div class="row">
-		<div class="large-2 columns">
-
-		</div>
-		<figure class="small-12 large-2 columns text-center">
-			<a href="#"><img src="<?php echo bloginfo('template_directory'); ?>/assets/images/demo/action-volunteer.png" alt=" ">
-			<figcaption>Volunteer</figcaption></a>
-		</figure>
-		<figure class="small-12 large-4 columns text-center">
-			<a href="#"><img src="<?php echo bloginfo('template_directory'); ?>/assets/images/demo/action-donation.png" alt=" ">
-			<figcaption>Donation</figcaption></a>
-		</figure>
-		<figure class="small-12 large-2 columns text-center">
-			<a href="#"><img src="<?php echo bloginfo('template_directory'); ?>/assets/images/demo/action-inkind.png" alt=" ">
-			<figcaption>In-kind donation</figcaption></a>
-		</figure>
-		<div class="large-2 columns">
-
-		</div>
-
-	</div>
-</section>
-
-<section>
-	<h2 class="text-center">Stay in touch</h2>
-	<div class="row">
-		<div class="small-12 medium-6 large-6 columns">
-			<ul class="menu centered vertical mainpage-info no-bullet align-center">
-				<li>
-					<a href="#"><i class="fa fa-fw fa-address-book green" aria-hidden="true"></i> <span>Program Contacts</span> &nbsp; <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
-				</li>
-				<li>
-					<a href="#"><i class="fa fa-fw fa-map-marker green" aria-hidden="true"></i><span>Site Locations</span> &nbsp; <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
-				</li>
-			</ul>
-		</div>
-		<div class="small-12 medium-6 large-6 columns subscribe-box">
-			<h3>Sign Up</h3>
-			<p>Receive news and invitations</p>
-			<label>Email
-        <input type="email" placeholder="Enter your email address">
-      </label>
-			<button type="button" name="button" class="button large">Subscribe</button>
-		</div>
-	</div>
-</section>
-
 <?php get_footer();
