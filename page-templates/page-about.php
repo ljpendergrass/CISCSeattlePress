7ab82fc88fc8a0451fd2acf50e598317c97fd711
ljpendergrass <logmister@gmail.com>
2017-10-24 11:02:56 -0700

    Visual header update for about us page template

---
+++ page-templates/page-about.php
@@ -0,0 +1,45 @@
+<?php
+/*
+Template Name: About Us
+*/
+get_header(); ?>
+
+<div id="page-full-width about-page" role="main">
+
+<?php do_action( 'foundationpress_before_content' ); ?>
+<?php while ( have_posts() ) : the_post(); ?>
+  <article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
+    <?php
+    // If a featured image is set, insert into layout and use Interchange
+    // to select the optimal image size per named media query.
+    if ( has_post_thumbnail( $post->ID ) ) : ?>
+    	<header id=" " class="row visual-header header-programs" role="banner" data-interchange="[<?php echo the_post_thumbnail_url('featured-small'); ?>, small], [<?php echo the_post_thumbnail_url('featured-medium'); ?>, medium], [<?php echo the_post_thumbnail_url('featured-large'); ?>, large], [<?php echo the_post_thumbnail_url('featured-xlarge'); ?>, xlarge]">
+        <!-- Title -->
+        <h1 class="text-left"><?php echo get_the_title(); ?></h1>
+    	</header>
+    <?php //todo: add elseif for no thumbnail/featured image
+  endif; ?>
+
+      <!-- Excerpt/intro -->
+      <h3 class="program-excerpt text-center"><?php
+      $id = get_the_id();
+      echo get_field("excerpt", $id, false);
+      ?></h3>
+
+      <?php do_action( 'foundationpress_page_before_entry_content' ); ?>
+      <div class="entry-content row align-center">
+        <?php the_content(); ?>
+      </div>
+      </div>
+
+      <?php do_action( 'foundationpress_page_before_comments' ); ?>
+      <?php comments_template(); ?>
+      <?php do_action( 'foundationpress_page_after_comments' ); ?>
+  </article>
+<?php endwhile;?>
+
+<?php do_action( 'foundationpress_after_content' ); ?>
+
+</div>
+
+<?php get_footer();
