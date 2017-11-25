603a458ebf55bc0ed1afc23e87b8d1b51c57755b
ljpendergrass <logmister@gmail.com>
2017-11-25 12:45:02 -0800

    headers updates

--- page-templates/page-about-generic.php
+++ page-templates/page-about-generic.php
@@ -7,7 +7,7 @@ get_header(); ?>
 
 <div role="main">
 
-<?php get_template_part( 'template-parts/visual-header' ); ?>
+<?php get_template_part( 'template-parts/visual-header-about' ); ?>
 <?php do_action( 'foundationpress_before_content' ); ?>
 <?php while ( have_posts() ) : the_post(); ?>
   <article <?php post_class('main-content row') ?> id="post-<?php the_ID(); ?>">
