683f3ab60210c3e41ff5e4ce3e399f21547ccf54
ljpendergrass <logmister@gmail.com>
2017-12-04 14:07:44 -0800

    About layout system

---
+++ library/about_layout.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * About Layout Detector for CISCSeattlePress
+ *
+ * @package WordPress
+ * @subpackage FoundationPress
+ * @since FoundationPress 2.6.0
+ */
+function about_layout() { // thank you to http://www.wpbeginner.com/wp-tutorials/how-to-show-recent-posts-by-category-in-wordpress/
+
+$args = [
+    'post_type' => 'page',
+    'fields' => 'ids',
+    'nopaging' => true,
+    'meta_key' => '_wp_page_template',
+    'meta_value' => 'page-templates/page-about-landing.php'
+];
+$posts = get_posts( $args );
+$id = $posts[0];
+
+$info = get_field("about_us_side_menu", $id, false); // info section from user
+
+$contents = array();
+$links = array();
+foreach ($info as $page){
+  $href = get_permalink($page);
+  $title = get_the_title($page);
+  $item = "<li><a href='{$href}'>{$title}</a></li>";
+  $contents[] = $item;
+}
+
+if (!($contents)) { // kludge contents formatting
+  $menu = ''; // else contents section is empty
+} else {
+  $menu = '<div class="card contents-card">
+  <ul class="contents no-bullet">
+  ' . implode("\n",$contents) . '
+  </ul>
+  </div>';
+}
+
+// default: full width
+$string = '<div class="small-12 columns">';
+
+if (!(empty($menu))) { // if menu exists
+  $string = '<div class="small-12 medium-4 large-4 columns">
+      ' . $menu . '
+      </div>
+    <div class="small-12 medium-8 large-7 columns">';
+};
+
+return $string;
+
+}
