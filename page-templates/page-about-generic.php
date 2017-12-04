d34ce7b32d8741ec19d0ad6a4fc27f9a0d19e747
ljpendergrass <logmister@gmail.com>
2017-12-04 14:07:27 -0800

    rearrange some code bits

--- page-templates/page-about-generic.php
+++ page-templates/page-about-generic.php
@@ -4,15 +4,15 @@ Template Name: About Us Generic
 */
 get_header(); ?>
 
-
 <div role="main">
 
+  <?php do_action( 'foundationpress_before_content' ); ?>
 <?php get_template_part( 'template-parts/visual-header-about' ); ?>
-<?php do_action( 'foundationpress_before_content' ); ?>
 <?php while ( have_posts() ) : the_post(); ?>
   <article <?php post_class('main-content row') ?> id="post-<?php the_ID(); ?>">
       <?php do_action( 'foundationpress_page_before_entry_content' ); ?>
-      <div class="entry-content">
+      <?php echo about_layout(); ?>
+      <!-- <div class="entry-content"> -->
           <?php the_content(); ?>
       </div>
       <footer>
